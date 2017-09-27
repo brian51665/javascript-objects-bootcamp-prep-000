928869b02da97bfcf6298ad2ed7942351232f6f2
brian51665 <bdavis51892@yahoo.com>
2017-09-27 01:44:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var myFirstObject = {breakfast : "Frozen Waffles"};
 
 var mySecondObject = new Object({dinner : "Lasagna"});
 
-var playlist = {};
+var playlist = {REM : 'loosing my religion', Foo Fighters : 'everlong', Gorillas : 'clint eastwood'};
